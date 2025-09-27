b4c2583d6bd2fa10b887ecc44b56431ce9e079b1
Katarina16004 <veljkovickatarina56@gmail.com>
2025-09-27 19:04:49 +0200

    manager authorization; initial navigation

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Veb_Projekat.Filters;
+using Veb_Projekat.Models.Enums;
+
+namespace Veb_Projekat.Controllers
+{
+    [AuthorizeSession(RoleEnum.Manager)]
+    public class ManagerController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateArrangement()
+        {
+            return View();
+        }
+
+        public ActionResult ApproveComments()
+        {
+            return View();
+        }
+    }
+}
